43420c0d1c3c7009286cb7f135a2202664136b56
Nick Przybylski <nickprzy1234@gmail.com>
2022-04-13 12:23:44 -0400

    added vpath as possible environment variable

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ wpath = sys.argv[1]
 config = sys.argv[2]
 dpath = os.environ.get('dpath')
 model = os.environ.get('model')
+vpath = os.environ.get('vpath')
 
 #print(wpath)
 params,model,config,util,wpath,path,fpath,n_iters = outer_sweep_window( wpath = wpath, config = config, model = model )
@@ -15,7 +16,7 @@ params,model,config,util,wpath,path,fpath,n_iters = outer_sweep_window( wpath = 
 
 for i,p in enumerate(params):
     print(f'ITER {i}: {n_iters}')
-    cmd = f'sbatch --export=i={i},p0={p[0]},p1={p[1]},model={model},config={config},util={util},wpath={wpath},path={path},fpath={fpath},n_iters={n_iters},dpath={dpath} inner.sh'
+    cmd = f'sbatch --export=i={i},p0={p[0]},p1={p[1]},model={model},config={config},util={util},wpath={wpath},path={path},fpath={fpath},n_iters={n_iters},dpath={dpath},vpath={vpath} inner.sh'
 
 #{i} {p[0]} {p[1]} {model} {config} {util} {wpath} {path} {fpath} {n_iters}'
 
