9e17629891f524ebc3bb0ec427d8d7b92f58ca1f
Nick Przybylski <nprzybylski@ac16.newmexicoconsortium.org>
2022-04-27 10:43:05 -0600

    added script for random runs

---
+++ script_rand.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source $wpath/window-env/bin/activate
+
+config="1sec_rand"
+
+for ((i=0; i<500; i+=1))
+do
+	sbatch --export=config="config/${config}.yaml",util=$util,wpath=$wpath,model=$model,vpath=$vpath,dpath=$dpath,run=$i submit.slurm
+done
